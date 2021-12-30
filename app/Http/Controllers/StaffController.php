596845dbf62cfe22b018cc65b8f50385525cc282
kingarthur11 <38188669+kingarthur11@users.noreply.github.com>
2021-12-30 13:37:52 +0100

    fixes

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -80,9 +80,9 @@ class StaffController extends Controller
             $request = Request::create('/oauth/token', 'POST', $body);
 
             $result = $this->app->handle($request);
-
-            return $result = json_decode($result->getContent(), true);
-           $response['token'] = $result['access_token'];
+            
+            $result = json_decode($result->getContent(), true);
+            $response['token'] = $result['access_token'];
             $response['refresh_token'] = $result['refresh_token'];
 
             $clientName = auth()->guard('user')->user()->first_name.' '.auth()->guard('user')->user()->last_name;
